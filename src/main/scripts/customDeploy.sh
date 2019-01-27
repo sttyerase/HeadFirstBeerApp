636328b7d34bc95a79eb6cc4e20cf81edd81e185
Dan Moore <sttyerase@gmail.com>
2019-01-27 23:52:50 +0000

    Add deployment to Maven configuration.  Custom deploy script.

--- src/main/scripts/customDeploy.sh
+++ src/main/scripts/customDeploy.sh
@@ -1,3 +1,8 @@
+## THIS SCRIPT was create to deploy the Maven build artifact (jar file) to the Tomcat
+## container provided in the Eclipse Che Maven-Tomcat-Java stack.  The directory locations
+## may be modified for other deployment locations.
+## The default locations in the script may be overridden by environment variables.
+
 export PROJECT_HOME=${PROJECT_HOME:-"/projects/HeadFirstBeerApp"}
 export TOMCAT_HOME=${TOMCAT_HOME:-"/home/user/tomcat8"}
 export DEPLOY_DIR=$TOMCAT_HOME/webapps
